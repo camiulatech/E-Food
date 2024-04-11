ad2720cbacaf8f9e2c8627467e6b635ed7d7ba41
Camila <caulate@estudiantec.cr>
2024-04-11 11:13:24 -0600

    Sprint2-Creacion tabla Tipo_Precio

---
+++ EFood.Modelos/Tipo_Precio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFood.Modelos
+{
+    public class Tipo_Precio
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Descripcion es Requerida")]
+        [MaxLength(100, ErrorMessage = "Descripcion debe ser Maximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+    }
+}
