aafd36312e3a735022d42e4303d42241f13dffa6
Camila <caulate@estudiantec.cr>
2024-04-11 12:07:07 -0600

    Sprint2-Creacion tabla PrecioProducto

---
+++ EFood.Modelos/PrecioProducto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFood.Modelos
+{
+    public class PrecioProducto
+    {
+        [Required(ErrorMessage = "ID del producto es requerido")]
+        public int IdProducto { get; set; }
+
+        [ForeignKey("IdProducto")]
+        public Producto Producto { get; set; }
+
+        [Required(ErrorMessage = "ID del tipo de precio es requerido")]
+        public int IdTipoPrecio { get; set; }
+
+        [ForeignKey("IdTipoPrecio")]
+        public TipoPrecio TipoPrecio { get; set; }
+
+        [Required(ErrorMessage = "Monto es requerido")]
+        public float Monto { get; set; }
+    }
+}
