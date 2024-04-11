bcc76ef0f2a00ddb3ee70264e7cb5934fd7432ef
Fabiana0744 <fabiana0744@hotmail.com>
2024-04-11 11:15:46 -0600

    Sprint2-Creación de las tablas LineaComida y Producto

---
+++ EFood.Modelos/Producto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFood.Modelos
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "Nombre debe tener máximo 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "ID de línea de comida es requerido")]
+        public int IdLineaComida { get; set; }
+
+        [ForeignKey("IdLineaComida")]
+        public LineaComida LineaComida { get; set; }
+
+        [Required(ErrorMessage = "Contenido es requerido")]
+        public string Contenido { get; set; }
+
+        public string UbicacionImagen { get; set; }
+
+        [Required(ErrorMessage = "Precio es requerido")]
+        public decimal Precio { get; set; }
+    }
+}
