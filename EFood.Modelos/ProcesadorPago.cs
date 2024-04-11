5362c7691e2d0da9569cb369d7e7f9db5dd86584
ycambro <yurgenmora03@gmail.com>
2024-04-11 12:00:50 -0600

    Sprint2-Cambio de tipo a la variable Estado en ProcesadorPago

--- EFood.Modelos/ProcesadorPago.cs
+++ EFood.Modelos/ProcesadorPago.cs
@@ -28,12 +28,18 @@ namespace EFood.Modelos
         public virtual TipoProcesadorPago IdTipo { get; set; }
 
         [Required(ErrorMessage = "El estado es requerido")]
-        public bool Estado { get; set; }
+        public EstadoProcesadorPago Estado { get; set; }
 
         [Required(ErrorMessage = "Es requerido especificar si requiere verificación")]
         public bool RequiereVerificacion { get; set; }
 
         [MaxLength(200, ErrorMessage = "El método debe ser máximo de 200 caracteres")]
         public string Metodo { get; set; }
+
+        public enum EstadoProcesadorPago
+        {
+            Activo,
+            Inactivo
+        }
     }
 }
