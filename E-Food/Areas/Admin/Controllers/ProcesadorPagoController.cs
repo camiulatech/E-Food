176ba26754c4cb378256d1693ffe66c627d5b716
Camila <caulate@estudiantec.cr>
2024-04-20 11:02:43 -0600

    Sprint2-Views y js Procesador de Pago

--- E-Food/Areas/Admin/Controllers/ProcesadorPagoController.cs
+++ E-Food/Areas/Admin/Controllers/ProcesadorPagoController.cs
@@ -1,5 +1,6 @@
 ﻿using EFood.AccesoDatos.Repositorio.IRepositorio;
 using EFood.Modelos;
+using EFood.Utilidades;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Food.Areas.Admin.Controllers
@@ -15,13 +16,107 @@ namespace E_Food.Areas.Admin.Controllers
             _unidadTrabajo = unidadTrabajo;
         }
 
-
-
-
-
         public IActionResult Index()
         {
             return View();
         }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            ProcesadorPago procesadorPago = new ProcesadorPago();
+
+            if (id == null)
+            {
+                //Crear nuevo Procesador
+                procesadorPago.Estado = true;
+                return View(procesadorPago);
+            }
+            //Editar procesador
+            procesadorPago = await _unidadTrabajo.ProcesadorPago.Obtener(id.GetValueOrDefault());
+            if (procesadorPago == null)
+            {
+                return NotFound();
+            }
+            return View(procesadorPago);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(ProcesadorPago procesadorPago)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (procesadorPago.Id == 0)
+                {
+                    await _unidadTrabajo.ProcesadorPago.Agregar(procesadorPago);
+                    TempData[DS.Exitosa] = "Procesador de pago creado exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.ProcesadorPago.Actualizar(procesadorPago);
+                    TempData[DS.Exitosa] = "Procesador de pago actualizado exitosamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al guardar Procesador de pago";
+            return View(procesadorPago);
+        }
+
+
+        #region API
+
+        [HttpGet]
+        public async Task<IActionResult> getAll()
+        {
+            var all = await _unidadTrabajo.ProcesadorPago.ObtenerTodos();
+            return Json(new { data = all });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            var procesadorPagoDB = await _unidadTrabajo.ProcesadorPago.Obtener(id);
+            if (procesadorPagoDB == null)
+            {
+                return Json(new { success = false, message = "Error al borrar el Procesador de pago" });
+            }
+            _unidadTrabajo.ProcesadorPago.Remover(procesadorPagoDB);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Procesador de pago borrado correctamente" });
+        }
+
+
+        [ActionName("NameValidate")]
+        public async Task<IActionResult> NameValidate(string procesador, int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.ProcesadorPago.ObtenerTodos();
+            if (id == 0)
+            {
+                valor = lista.Any(p => p.Procesador.ToLower().Trim() == procesador.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(p => p.Procesador.ToLower().Trim() == procesador.ToLower().Trim() && p.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new { data = false });
+
+        }
+
+
+
+        #endregion
+
+
+
+
     }
 }
---
+++ E-Food/wwwroot/js/procesadorpago.js
@@ -0,0 +1,90 @@
+﻿let datatable;
+$(document).ready(function () {
+    loadDatatable();
+
+});
+
+
+function loadDatatable() {
+    datatable = $('#tblData').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ Registros Por Pagina",
+            "zeroRecords": "Ningun Registro",
+            "info": "Mostrar page _PAGE_ de _PAGES_",
+            "infoEmpty": "no hay registros",
+            "infoFiltered": "(filtered from _MAX_ total registros)",
+            "search": "Buscar",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "ajax": {
+            "url": "/Admin/PaymentProcessor/getAll"
+        },
+
+        "columns": [
+            { "data": "id" },
+            { "data": "processor" },
+            { "data": "type" },
+            {
+                "data": "status",
+                "render": function (data, type, row) {
+                    return data ? 'sí' : 'no';
+                }
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/Admin/PaymentProcessorCard/index/${data}" class="btn btn-primary text-white" style="cursor:pointer">
+                                <i class="bi bi-credit-card-fill"></i>
+                            </a>
+                            <a href="/Admin/PaymentProcessor/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                            <a onclick=Delete("/Admin/PaymentProcessor/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                <i class="bi bi-trash3-fill"></i>
+                            </a>
+                        </div>
+                    `;
+
+                }, "width": "20%"
+
+            }
+        ]
+    });
+}
+
+
+
+function Delete(url) {
+    swal({
+        title: "Esta seguro de Eliminar el Procesador de pago?",
+        text: "Este proceso es irreversible!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((erase) => {
+        if (erase) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        datatable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+
+
+}
