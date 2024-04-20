6b63a3842758db23a9f18ecce29cabca9be476d8
Camila <caulate@estudiantec.cr>
2024-04-20 10:15:20 -0600

    Sprint2-Controller Procesador Paogo

---
+++ E-Food/Areas/Admin/Controllers/ProcesadorPagoController.cs
@@ -0,0 +1,27 @@
+﻿using EFood.AccesoDatos.Repositorio.IRepositorio;
+using EFood.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Food.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProcesadorPagoController : Controller
+    {
+        
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public ProcesadorPagoController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
