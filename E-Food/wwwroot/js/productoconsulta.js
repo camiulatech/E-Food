2b01567c046de12aba2b26feb74672b2f660664f
Camila <caulate@estudiantec.cr>
2024-05-09 09:43:11 -0600

    Sprint4-Sin eliminar y sin upsert

---
+++ E-Food/wwwroot/js/productoconsulta.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    $('#tblDatos').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar MENU Registros Por Pagina",
+            "zeroRecords": "Ningun Registro",
+            "info": "Mostrar page PAGE de PAGES",
+            "infoEmpty": "no hay registros",
+            "infoFiltered": "(filtered from MAX total registros)",
+            "search": "Buscar",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "ajax": {
+            "url": "/Admin/Producto/ObtenerTodos"
+        },
+        "columns": [
+            { "data": "id", "width": "20%" },
+            { "data": "nombre", "width": "20%" },
+            { "data": "lineaComida.nombre", "width": "20%" }
+        ]
+
+    });
+}
