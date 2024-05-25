c4bd17b94a98702c2f78f9c769bea1de99d9a35f
Camila <caulate@estudiantec.cr>
2024-05-25 11:20:15 -0600

    Sprint5-Modelo y Repositorio Carrito de Compra e item Carrito de Compra

---
+++ EFood.AccesoDatos/Repositorio/IRepositorio/ICarritoCompraRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using EFood.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFood.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface ICarritoCompraRepositorio
+    {
+        void AgregarItem(Producto producto, int cantidad,TipoPrecio tipoPrecio);
+
+        void EliminarItem(Producto producto);
+
+        void Limpiar();
+
+        decimal ObtenerPrecio();
+    }
+}
