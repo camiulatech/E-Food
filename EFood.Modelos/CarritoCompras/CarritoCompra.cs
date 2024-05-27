2151099388109d426576f631c8df16cd0d9589e1
ycambro <yurgenmora03@gmail.com>
2024-05-27 09:15:34 -0600

    Sprint5 - CarritoCompra Controller y Vista creados

--- EFood.Modelos/CarritoCompras/CarritoCompra.cs
+++ EFood.Modelos/CarritoCompras/CarritoCompra.cs
@@ -9,5 +9,58 @@ namespace EFood.Modelos.CarritoCompras
     public class CarritoCompra
     {
         public List<ItemCarritoCompra> itemCarritoCompras { get; set; }
+
+        public void AgregarItem(Producto producto, int cantidad, TipoPrecio tipoPrecio)
+        {
+            // Verificar si el producto ya está en el carrito
+            var itemExistente = itemCarritoCompras.FirstOrDefault(i => i.Producto.Id == producto.Id);
+
+            if (itemExistente != null)
+            {
+                // Si el producto ya está en el carrito, actualizamos la cantidad
+                itemExistente.Cantidad += cantidad;
+            }
+            else
+            {
+                // Si el producto no está en el carrito, lo añadimos como un nuevo elemento
+                itemCarritoCompras.Add(new ItemCarritoCompra
+                {
+                    Producto = producto,
+                    Cantidad = cantidad,
+                    TipoPrecio = tipoPrecio
+                });
+            }
+        }
+
+        public void EliminarItem(Producto producto)
+        {
+            // Eliminar el producto del carrito
+            var item = itemCarritoCompras.FirstOrDefault(i => i.Producto.Id == producto.Id);
+            if (item != null)
+            {
+                itemCarritoCompras.Remove(item);
+            }
+        }
+
+        public void Limpiar()
+        {
+            // Limpiar el carrito
+            itemCarritoCompras.Clear();
+        }
+
+        public decimal ObtenerPrecio()
+        {
+            // Calcular el precio total sumando los precios de todos los productos en el carrito
+            decimal precioTotal = 0;
+            if (itemCarritoCompras == null)
+            {
+                return precioTotal;
+            }
+            foreach (var item in itemCarritoCompras)
+            {
+                precioTotal = precioTotal + ((item.Producto.Monto + (item.Producto.Monto * item.TipoPrecio.Cambio)) * item.Cantidad);
+            }
+            return precioTotal;
+        }
     }
 }
--- EFoodCommerce/Areas/Commerce/Controllers/CarritoCompraController.cs
+++ EFoodCommerce/Areas/Commerce/Controllers/CarritoCompraController.cs
@@ -5,6 +5,7 @@ using EFood.Modelos.ViewModels;
 using EFood.Modelos;
 using EFood.Utilidades;
 using EFood.Modelos.CarritoCompras;
+using Newtonsoft.Json;
 
 
 namespace EFoodCommerce.Areas.Commerce.Controllers
@@ -14,12 +15,6 @@ namespace EFoodCommerce.Areas.Commerce.Controllers
     {
 
         private const string SessionKeyCarrito = "Carrito";
-        private readonly IUnidadTrabajo _unidadTrabajo;
-
-        public CarritoCompraController(IUnidadTrabajo unidadTrabajo)
-        {
-            _unidadTrabajo = unidadTrabajo;
-        }
 
         public IActionResult Index()
         {
@@ -49,17 +44,17 @@ namespace EFoodCommerce.Areas.Commerce.Controllers
             return carritoJson == null ? new CarritoCompra() : JsonConvert.DeserializeObject<CarritoCompra>(carritoJson);
         }
 
-        private void GuardarCarritoEnSesion(Carrito carrito)
+        private void GuardarCarritoEnSesion(CarritoCompra carrito)
         {
             var carritoJson = JsonConvert.SerializeObject(carrito);
             HttpContext.Session.SetString(SessionKeyCarrito, carritoJson);
         }
 
-        private Producto ObtenerProductoPorId(int productoId)
-        {
-            // Implementa este método para obtener el producto de tu base de datos
-            return new Producto { Id = productoId, Nombre = "Producto de ejemplo", Precio = 10.0m };
-        }
+        //private Producto ObtenerProductoPorId(int productoId)
+        //{
+        //    // Implementa este método para obtener el producto de tu base de datos
+        //    return new Producto { Id = productoId, Nombre = "Producto de ejemplo", Precio = 10.0m };
+        //}
 
 
         #region API
