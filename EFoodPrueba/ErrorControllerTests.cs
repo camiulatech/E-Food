755d70daa31290210672006eddc2849e9320c046
Fabiana0744 <fabiana0744@hotmail.com>
2024-05-29 15:42:13 -0600

    Sprint5-Pruebas de ErrorController finalizadas

--- EFoodPrueba/ErrorControllerTests.cs
+++ EFoodPrueba/ErrorControllerTests.cs
@@ -1,31 +1,28 @@
 ﻿using E_Food.Areas.Admin.Controllers;
 using EFood.AccesoDatos.Repositorio.IRepositorio;
 using EFood.Modelos;
-using EFood.Utilidades;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace E_Food.Tests
 {
     [TestFixture]
-    public class BitacoraControllerTests
+    public class ErrorControllerTests
     {
         private Mock<IUnidadTrabajo> _unidadTrabajoMock;
-        private BitacoraController _controller;
+        private ErrorController _controller;
 
         [SetUp]
         public void Setup()
         {
             _unidadTrabajoMock = new Mock<IUnidadTrabajo>();
 
-            _controller = new BitacoraController(_unidadTrabajoMock.Object);
+            _controller = new ErrorController(_unidadTrabajoMock.Object);
         }
 
         [TearDown]
@@ -44,14 +41,12 @@ namespace E_Food.Tests
             Assert.IsInstanceOf<ViewResult>(result);
         }
 
-
-
         [Test]
-        public async Task ObtenerTodos_RetornarJsonConLosRegistrosDeBitacora()
+        public async Task ObtenerTodos_DeberiaRetornarJsonConTodosLosRegistrosDeError()
         {
             // Arrange
-            var registrosMock = new List<Bitacora>(); // Simula una lista de registros de bitácora
-            _unidadTrabajoMock.Setup(u => u.Bitacora.ObtenerTodos(It.IsAny<Expression<Func<Bitacora, bool>>>(), It.IsAny<Func<IQueryable<Bitacora>, IOrderedQueryable<Bitacora>>>(), It.IsAny<string>(), It.IsAny<bool>())).ReturnsAsync(registrosMock);
+            var registrosMock = new List<Error>(); // Simula una lista de registros de error
+            _unidadTrabajoMock.Setup(u => u.Error.ObtenerTodos(It.IsAny<Expression<Func<Error, bool>>>(), It.IsAny<Func<IQueryable<Error>, IOrderedQueryable<Error>>>(), It.IsAny<string>(), It.IsAny<bool>())).ReturnsAsync(registrosMock);
 
             // Act
             var result = await _controller.ObtenerTodos() as JsonResult;
@@ -62,14 +57,13 @@ namespace E_Food.Tests
             // Verifica la estructura del JSON retornado según lo esperado
         }
 
-
         [Test]
-        public async Task ObtenerPorFecha_RetornarJsonConLosRegistrosDeBitacoraDeUnaFechaEspecifica()
+        public async Task ObtenerPorFecha_DeberiaRetornarJsonConLosRegistrosDeErrorDeUnaFechaEspecifica()
         {
             // Arrange
             var fecha = new DateTime(2024, 5, 29); // Puedes establecer una fecha específica para la prueba
-            var registrosMock = new List<Bitacora>(); // Simula una lista de registros de bitácora
-            _unidadTrabajoMock.Setup(u => u.Bitacora.ObtenerPorFecha(fecha)).ReturnsAsync(registrosMock);
+            var registrosMock = new List<Error>(); // Simula una lista de registros de error
+            _unidadTrabajoMock.Setup(u => u.Error.ObtenerPorFecha(fecha)).ReturnsAsync(registrosMock);
 
             // Act
             var result = await _controller.ObtenerPorFecha(fecha) as JsonResult;
@@ -79,7 +73,5 @@ namespace E_Food.Tests
             Assert.IsNotNull(result.Value);
             // Verifica la estructura del JSON retornado según lo esperado
         }
-
-
     }
 }
