1a334d8b5c046c2766697226bae4d39d6e49a8d4
Fabiana0744 <fabiana0744@hotmail.com>
2024-06-13 09:56:32 -0600

    Sprint6-Arreglo de imagenes y deploy del proyecto

--- EFood.Utilidades/DS.cs
+++ EFood.Utilidades/DS.cs
@@ -10,7 +10,7 @@ namespace EFood.Utilidades
     {
         public const string Exitosa = "Exitosa";
         public const string Error = "Error";
-        public const string ImagenRuta = "https://efoodimages.blob.core.windows.net/productos/imagenes/";
+        public const string ImagenRuta = "https://efoodimagenes.blob.core.windows.net/productos/imagenes/";
 
         public const string Rol_Admin = "Admin";
         public const string Rol_Seguridad = "Seguridad";
