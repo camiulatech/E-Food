e130cf67c4010cb19a4d4d1ed547bc0a441b8254
Fabiana0744 <fabiana0744@hotmail.com>
2024-06-14 10:53:57 -0600

    Sprint6- Cambios en nombres de pruebas y eliminación de comentarios

--- EFoodPrueba/TarjetaControllerTests.cs
+++ EFoodPrueba/TarjetaControllerTests.cs
@@ -1,14 +1,11 @@
 ﻿using System.Security.Claims;
-using System.Threading.Tasks;
 using E_Food.Areas.Admin.Controllers;
 using EFood.AccesoDatos.Repositorio.IRepositorio;
 using EFood.Modelos;
 using EFood.Utilidades;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using NUnit.Framework;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System.Security.Principal;
 
@@ -52,17 +49,17 @@ namespace E_Food.Tests
         [Test]
         public async Task Upsert_Post_Crea_Nueva_Tarjeta_Redirecciona_A_Index()
         {
-            // Arrange
+            
             _unidadTrabajoMock.Setup(u => u.Tarjeta.Agregar(It.IsAny<Tarjeta>())).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Guardar()).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
 
             var nuevaTarjeta = new Tarjeta { Id = 0, Nombre = "Nueva Tarjeta" };
 
-            // Act
+            
             var result = await _controller.Upsert(nuevaTarjeta);
 
-            // Assert
+            
             Assert.IsInstanceOf<RedirectToActionResult>(result);
             var redirectResult = result as RedirectToActionResult;
             Assert.NotNull(redirectResult);
@@ -72,17 +69,17 @@ namespace E_Food.Tests
         [Test]
         public async Task Upsert_Post_Crea_Nueva_Tarjeta_Llama_Agregar_Y_Guardar()
         {
-            // Arrange
+            
             _unidadTrabajoMock.Setup(u => u.Tarjeta.Agregar(It.IsAny<Tarjeta>())).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Guardar()).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
 
             var nuevaTarjeta = new Tarjeta { Id = 0, Nombre = "Nueva Tarjeta" };
 
-            // Act
+            
             await _controller.Upsert(nuevaTarjeta);
 
-            // Assert
+            
             _unidadTrabajoMock.Verify(u => u.Tarjeta.Agregar(It.IsAny<Tarjeta>()), Times.Once);
             _unidadTrabajoMock.Verify(u => u.Guardar(), Times.Exactly(2));
         }
@@ -90,51 +87,51 @@ namespace E_Food.Tests
         [Test]
         public async Task Upsert_Post_Crea_Nueva_Tarjeta_Registra_Bitacora()
         {
-            // Arrange
+            
             _unidadTrabajoMock.Setup(u => u.Tarjeta.Agregar(It.IsAny<Tarjeta>())).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Guardar()).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
 
             var nuevaTarjeta = new Tarjeta { Id = 0, Nombre = "Nueva Tarjeta" };
 
-            // Act
+            
             await _controller.Upsert(nuevaTarjeta);
 
-            // Assert
+            
             _unidadTrabajoMock.Verify(u => u.Bitacora.RegistrarBitacora("testuser", "0", "Se insertó la tarjeta 'Nueva Tarjeta' con ID: 0"), Times.Once);
         }
 
         [Test]
         public async Task Upsert_Post_Crea_Nueva_Tarjeta_Establece_TempData_Exitosa()
         {
-            // Arrange
+            
             _unidadTrabajoMock.Setup(u => u.Tarjeta.Agregar(It.IsAny<Tarjeta>())).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Guardar()).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
 
             var nuevaTarjeta = new Tarjeta { Id = 0, Nombre = "Nueva Tarjeta" };
 
-            // Act
+            
             await _controller.Upsert(nuevaTarjeta);
 
-            // Assert
+            
             _tempDataMock.VerifySet(t => t[DS.Exitosa] = "Tarjeta creada exitosamente", Times.Once);
         }
 
         [Test]
         public async Task Upsert_Post_Actualiza_Tarjeta_Existente_Redirecciona_A_Index()
         {
-            // Arrange
+            
             _unidadTrabajoMock.Setup(u => u.Tarjeta.Actualizar(It.IsAny<Tarjeta>()));
             _unidadTrabajoMock.Setup(u => u.Guardar()).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
 
             var existingTarjeta = new Tarjeta { Id = 1, Nombre = "Tarjeta Existente" };
 
-            // Act
+            
             var result = await _controller.Upsert(existingTarjeta);
 
-            // Assert
+            
             Assert.IsInstanceOf<RedirectToActionResult>(result);
             var redirectResult = result as RedirectToActionResult;
             Assert.NotNull(redirectResult);
@@ -144,17 +141,17 @@ namespace E_Food.Tests
         [Test]
         public async Task Upsert_Post_Actualiza_Tarjeta_Existente_Llama_Actualizar_Y_Guardar()
         {
-            // Arrange
+            
             _unidadTrabajoMock.Setup(u => u.Tarjeta.Actualizar(It.IsAny<Tarjeta>()));
             _unidadTrabajoMock.Setup(u => u.Guardar()).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
 
             var existingTarjeta = new Tarjeta { Id = 1, Nombre = "Tarjeta Existente" };
 
-            // Act
+            
             await _controller.Upsert(existingTarjeta);
 
-            // Assert
+            
             _unidadTrabajoMock.Verify(u => u.Tarjeta.Actualizar(It.IsAny<Tarjeta>()), Times.Once);
             _unidadTrabajoMock.Verify(u => u.Guardar(), Times.Once);
         }
@@ -162,57 +159,54 @@ namespace E_Food.Tests
         [Test]
         public async Task Upsert_Post_Actualiza_Tarjeta_Existente_Registra_Bitacora()
         {
-            // Arrange
+            
             _unidadTrabajoMock.Setup(u => u.Tarjeta.Actualizar(It.IsAny<Tarjeta>()));
             _unidadTrabajoMock.Setup(u => u.Guardar()).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
 
             var existingTarjeta = new Tarjeta { Id = 1, Nombre = "Tarjeta Existente" };
 
-            // Act
+            
             await _controller.Upsert(existingTarjeta);
 
-            // Assert
+            
             _unidadTrabajoMock.Verify(u => u.Bitacora.RegistrarBitacora("testuser", "1", "Se actualizó la tarjeta 'Tarjeta Existente' con ID: 1"), Times.Once);
         }
 
         [Test]
         public async Task Upsert_Post_Actualiza_Tarjeta_Existente_Establece_TempData_Exitosa()
         {
-            // Arrange
+            
             _unidadTrabajoMock.Setup(u => u.Tarjeta.Actualizar(It.IsAny<Tarjeta>()));
             _unidadTrabajoMock.Setup(u => u.Guardar()).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
 
             var existingTarjeta = new Tarjeta { Id = 1, Nombre = "Tarjeta Existente" };
 
-            // Act
+            
             await _controller.Upsert(existingTarjeta);
 
-            // Assert
+            
             _tempDataMock.VerifySet(t => t[DS.Exitosa] = "Tarjeta actualizada exitosamente", Times.Once);
         }
 
         [Test]
-        public async Task Eliminar_Post_With_Valid_Id_Returns_JsonResult()
+        public async Task Eliminar_Post_Con_Id_Valido_Returna_ResultadoJson()
         {
-            // Arrange
+            
             int validId = 1;
             var existingTarjeta = new Tarjeta { Id = validId, Nombre = "Tarjeta Existente" };
             _unidadTrabajoMock.Setup(u => u.Tarjeta.Obtener(validId)).ReturnsAsync(existingTarjeta);
             _unidadTrabajoMock.Setup(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
 
-            // Simular la identidad del usuario
             var identity = new GenericIdentity("testuser");
             var principal = new ClaimsPrincipal(identity);
 
-            // Configurar HttpContext
             var httpContext = new DefaultHttpContext
             {
                 User = principal
             };
 
-            // Configurar ControllerContext
             var controllerContext = new ControllerContext
             {
                 HttpContext = httpContext
@@ -220,34 +214,31 @@ namespace E_Food.Tests
 
             _controller.ControllerContext = controllerContext;
 
-            // Act
+            
             var result = await _controller.Eliminar(validId);
 
-            // Assert
+            
             Assert.IsInstanceOf<JsonResult>(result);
         }
 
         [Test]
-        public async Task Eliminar_Post_With_Valid_Id_Calls_Remover_And_Guardar()
+        public async Task Eliminar_Post_Con_Id_Valido_Llama_Remover_Y_Guardar()
         {
-            // Arrange
+            
             int validId = 1;
             var existingTarjeta = new Tarjeta { Id = validId, Nombre = "Tarjeta Existente" };
             _unidadTrabajoMock.Setup(u => u.Tarjeta.Obtener(validId)).ReturnsAsync(existingTarjeta);
             _unidadTrabajoMock.Setup(u => u.Guardar()).Returns(Task.CompletedTask);
             _unidadTrabajoMock.Setup(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
 
-            // Simular la identidad del usuario
             var identity = new GenericIdentity("testuser");
             var principal = new ClaimsPrincipal(identity);
 
-            // Configurar HttpContext
             var httpContext = new DefaultHttpContext
             {
                 User = principal
             };
 
-            // Configurar ControllerContext
             var controllerContext = new ControllerContext
             {
                 HttpContext = httpContext
@@ -255,10 +246,10 @@ namespace E_Food.Tests
 
             _controller.ControllerContext = controllerContext;
 
-            // Act
+            
             await _controller.Eliminar(validId);
 
-            // Assert
+            
             _unidadTrabajoMock.Verify(u => u.Tarjeta.Remover(existingTarjeta), Times.Once);
             _unidadTrabajoMock.Verify(u => u.Guardar(), Times.Once);
             _unidadTrabajoMock.Verify(u => u.Bitacora.RegistrarBitacora(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
