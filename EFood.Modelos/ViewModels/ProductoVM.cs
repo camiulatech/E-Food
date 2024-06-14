cca8182246db02461a5764cb1744b3367049ad47
Fabiana0744 <fabiana0744@hotmail.com>
2024-06-14 09:20:46 -0600

    Sprint6-Se quitaron comentarios innecesarios de los viewmodels y carrito de compras

--- EFood.Modelos/ViewModels/ProductoVM.cs
+++ EFood.Modelos/ViewModels/ProductoVM.cs
@@ -1,10 +1,4 @@
-﻿using EFood.Modelos.CarritoCompras;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EFood.Modelos.ViewModels
 {
