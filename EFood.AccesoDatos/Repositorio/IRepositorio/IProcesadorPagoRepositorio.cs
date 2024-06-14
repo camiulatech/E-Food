7dd5f617ceb36faf4e3a427e1209c7b8754f364e
Camila <caulate@estudiantec.cr>
2024-06-14 09:22:02 -0600

    Sprint6-Cambios Formato IRepositorio

--- EFood.AccesoDatos/Repositorio/IRepositorio/IProcesadorPagoRepositorio.cs
+++ EFood.AccesoDatos/Repositorio/IRepositorio/IProcesadorPagoRepositorio.cs
@@ -1,10 +1,4 @@
 ﻿using EFood.Modelos;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EFood.AccesoDatos.Repositorio.IRepositorio
 {
--- EFood.AccesoDatos/Repositorio/IRepositorio/IProductoRepositorio.cs
+++ EFood.AccesoDatos/Repositorio/IRepositorio/IProductoRepositorio.cs
@@ -1,10 +1,5 @@
 ﻿using EFood.Modelos;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EFood.AccesoDatos.Repositorio.IRepositorio
 {
