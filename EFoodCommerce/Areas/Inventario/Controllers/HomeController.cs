082f274a47f04c6480d4fb06291eefc276fd3438
ycambro <yurgenmora03@gmail.com>
2024-06-13 21:01:53 -0600

    Sprint6-Inicio actualizado y añadido filtro por linea de comida al inicio.

--- EFoodCommerce/Areas/Inventario/Controllers/HomeController.cs
+++ EFoodCommerce/Areas/Inventario/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using EFood.AccesoDatos.Repositorio.IRepositorio;
+using EFood.Modelos;
 using EFood.Modelos.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,15 +11,35 @@ namespace EFood.Areas.Inventario.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnidadTrabajo _unidadTrabajo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnidadTrabajo unidadTrabajo)
         {
             _logger = logger;
+            _unidadTrabajo = unidadTrabajo;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? idLineaComida)
         {
-            return View();
+            var productoVM = new ProductoVM();
+
+            // Obtener la lista de líneas de comida
+            productoVM.LineaComidaLista = _unidadTrabajo.Producto.ObtenerLineasComidasListaDesplegable("LineaComida");
+
+            if (idLineaComida.HasValue)
+            {
+                // Si se proporciona un ID de línea de comida, filtrar los productos por esa línea
+                productoVM.Productos = await _unidadTrabajo.Producto.FiltrarPorLineaComida(idLineaComida.Value);
+            }
+            else
+            {
+                // Si no se proporciona un ID de línea de comida, cargar todos los productos
+                productoVM.Productos = await _unidadTrabajo.Producto.ObtenerTodos(incluirPropiedades: "LineaComida");
+            }
+            productoVM.LineaComidaSeleccionadaId = idLineaComida;
+            return View(productoVM);
+            //IEnumerable<Producto> productoLista = await _unidadTrabajo.Producto.ObtenerTodos();
+            //return View(productoLista);
         }
 
         public IActionResult Ayuda()
@@ -29,6 +51,18 @@ namespace EFood.Areas.Inventario.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FiltrarProductos(int idLineaComida)
+        {
+            if (idLineaComida == 0)
+            {
+                var productos = await _unidadTrabajo.Producto.ObtenerTodos(incluirPropiedades: "LineaComida");
+                return PartialView("_ProductosParciales", productos);
+            }
+            var productosFiltrados = await _unidadTrabajo.Producto.FiltrarPorLineaComida(idLineaComida);
+            return PartialView("_ProductosParciales", productosFiltrados);
         }
     }
 }
